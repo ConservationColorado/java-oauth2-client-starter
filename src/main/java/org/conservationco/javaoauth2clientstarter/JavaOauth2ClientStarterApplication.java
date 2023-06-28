5d0bba925cccccd53c7de41fbf21dceaeae6a7fa
Oliver Abdulrahim <oliver.abdulrahim@gmail.com>
2023-06-27 19:34:38 -0600

    Initial commit: start.spring.io

---
+++ src/main/java/org/conservationco/javaoauth2clientstarter/JavaOauth2ClientStarterApplication.java
@@ -0,0 +1,13 @@
+package org.conservationco.javaoauth2clientstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOauth2ClientStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOauth2ClientStarterApplication.class, args);
+	}
+
+}
